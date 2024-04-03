5e9d79418b18ee2ed158f6127442a81928099fd9
Kshitiz Adhikari <adhikarikshitiz12@gmail.com>
2024-04-03 17:22:49 +0545

    birthdayCakesAndCandles

---
+++ birthdayCakesAndCandles.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function birthdayCakeCandles(candles) {
+    let max = candles[0];
+    let count = 0;
+
+    for(let i=0; i<candles.length; i++) {
+        if(candles[i] > max) {
+            max=candles[i];
+            count = 1;
+        } else if(candles[i] == max) {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+let candles = [4, 4, 1, 3];
+console.log(birthdayCakeCandles(candles));
