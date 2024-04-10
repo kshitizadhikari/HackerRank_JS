bda2d29c276d7471ee13445cda3ea02e818a4238
Kshitiz Adhikari <adhikarikshitiz12@gmail.com>
2024-04-10 12:25:49 +0545

    getTotalX fix

--- getTotalX.js
+++ getTotalX.js
@@ -1,24 +1,38 @@
 function getTotalX(a, b) {
-    let x = a.concat(b);
-    let count = 0;
-
-    for(let i=0; i<a.length; i++) {
-        for(let j=0; j<x.length; j++) {
-            if(x[j]%a[i] != 0) {
-                break;
-            }
-
-            if(j==x.length-1){
-                count++;
+    /*
+    1. Find all the factors of b say x;
+    2. Check if x is factor of a;
+    */
+    let result = 0;
+    let factors = [];
+    for (let i = 1; i <= b[0]; i++) {
+        let flag = 0;
+        for (let j = 0; j < b.length; j++) {
+            if (b[j] % i !== 0) {
+                flag = 1;
             }
         }
+        if (flag == 0) {
+            factors.push(i);
+        }
     }
-
-    
-
+    console.log(factors);
+    for (let i = 0; i < factors.length; i++) {
+        let flag = 0;
+        for (let j = 0; j < a.length; j++) {
+            if (factors[i] % a[j] !== 0) {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0) {
+            result++;
+        }
+    }
+    console.log(result);
+    return result;
 }
 
-
-let a = [2, 6];
-let b = [24, 36];
-console.log(getTotalX(a, b));
+let a = [2, 4];
+let b = [16, 32, 96];
+getTotalX(a, b);
