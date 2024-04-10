22b228be18d372ff82300eac6397b1b6f257b607
Kshitiz Adhikari <adhikarikshitiz12@gmail.com>
2024-04-10 14:40:26 +0545

    birthday fix

--- birthday.js
+++ birthday.js
@@ -1,17 +1,27 @@
 function birthday(s, d, m) {
-    let hashSet = new Set();
-    let count = 0;
-    for(let i=0; i<s.length; i++) {
-        let x = s[i];
-        let target = d - x;
+    /*
+    chocolate length = n;
+    length of segment = ron's birth month say x
+    sum of int on segment = x^2
+    */
 
-        if(hashSet.has(target)) {
-            count++;
-        } else {
-            hashSet.add(s[i]);
+    let lp = 0;
+    let rp = m - 1;
+    let count = 0;
+    while (rp < s.length) {
+        let sum = 0;
+        for (let i = lp; i <= rp; i++) {
+            sum += s[i];
         }
+        if (sum == d) {
+            count++;
+        }
+
+        lp++;
+        rp++;
     }
+    console.log(count);
     return count;
 }
 
-console.log(birthday([2, 2, 1, 3, 2], 4, 2));
+birthday([1, 2, 1, 3, 2], 3, 2);
