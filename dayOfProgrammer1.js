92cdcdccaff13f8921537058525cfb2f56340b29
Kshitiz Adhikari <adhikarikshitiz12@gmail.com>
2024-04-19 18:45:57 +0545

    dayOfProgrammer1

---
+++ dayOfProgrammer1.js
@@ -0,0 +1,21 @@
+"use strict";
+function dayOfProgrammer(year) {
+    //till 1917 JC
+    //after 1919 GC
+
+    if (year < 1918) {
+        if (year % 4 == 0) {
+            //leap year in JC
+            return `12.09.${year}`;
+        }
+    } else if (year >= 1919) {
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            return `12.09.${year}`;
+        }
+    } else if (year == 1918) {
+        return `26.09.${year}`;
+    }
+    return `13.09.${year}`;
+}
+
+console.log(dayOfProgrammer(1800));
